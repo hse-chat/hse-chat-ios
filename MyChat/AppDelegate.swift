4fb9b555c34ab80f702913a8e2bc24995439e5a5
blkshdw <aleksey.glebow@gmail.com>
2016-06-17 09:26:51 +0300

    fix

--- MyChat/AppDelegate.swift
+++ MyChat/AppDelegate.swift
@@ -38,6 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(application: UIApplication) {
+        ConnectionManager.sharedInstance.disconnect()
     }
 
 
